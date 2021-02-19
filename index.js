09e58d8589a617c163dcfe9acd2bfa70ad7ae8dd
siebtel <siebtel.ss@gmail.com>
2021-02-19 20:44:02 -0300

    merge same eid

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ const config = {
 var csv = fs.readFileSync('input.csv', 'utf8');
 var json = Papa.parse(csv, config);
 list_of_input = json['data'];
+
+//merge_eid
+function customizer(objValue, srcValue) {
+	let list = [];
+	if (ld.isArray(objValue)){
+		objValue.push(srcValue);
+		return [...new Set(objValue)];
+	}else if (ld.isArray(srcValue)){
+		srcValue.push(objValue);
+		return [...new Set(srcValue)];
+	}else if(objValue !== srcValue){
+		list.push(objValue, srcValue);
+		return [...new Set(list)];
+	}
+}
+
+for (let i = 0; i < list_of_input.length - 1; i++) {
+	let j = i+1;
+	while (j < list_of_input.length) {
+		if (list_of_input[i]['eid'] == list_of_input[j]['eid']){
+			ld.mergeWith(list_of_input[i], list_of_input[j], customizer);
+			list_of_input.splice(j,1);
+		}else{
+			j++;
+		}
+	}
+}
